cf6aed69a42427c447e48997279479fb484d6e10
snowkeerie <132265991+snowkeerie@users.noreply.github.com>
2023-05-16 13:33:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,7 @@ except URLError as e:
 #streamlit.write('The user entered ', fruit_choice)
 
 
-
-
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 streamlit.stop()
 
